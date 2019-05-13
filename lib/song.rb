1985d9f1c6dbc6a6910d464bf2fd178ae4cd4931
Rachele Suy <rachelesuy@gmail.com>
2019-05-13 16:26:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,14 @@ class Song
     song.artist_name = (name.split(" - ")[0])
     song
   end
+  
+  def self.create_from_filename(name)
+    song =self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+     @@all << song
+    song
+  end
   def self.destroy_all
     self.all.clear
   end
