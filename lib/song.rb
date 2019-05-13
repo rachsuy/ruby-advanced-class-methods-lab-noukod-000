b93c5698f99be443dcb0d349433184ac1502b209
Rachele Suy <rachelesuy@gmail.com>
2019-05-13 14:07:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,10 @@ end
 def self.find_or_create_by_name(name)
     self.all.find{|song| song.name == name}
   end
+  
+def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
 end
 song = Song.create
 Song.all.include?(song)
